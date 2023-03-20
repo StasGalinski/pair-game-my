db06426d8a48c14a3e1bda37bc83116af907c4fb
Stanislav Galinski <stas.galinski@gmail.com>
2023-03-20 11:56:37 +0200

    updated turns text

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
   return (
     <div className="App">
       <h1>Pair Game</h1>
-      <h2>Turn : {turns}</h2>
+      <h2>Number of turns : {turns}</h2>
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-field">
         {cards.map((card) => (
